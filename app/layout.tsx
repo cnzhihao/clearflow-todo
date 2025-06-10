755c53beb71984707e5f2426c8bc9e30bd652b88
cnzhihao <zhcnzhihao@gmail.com>
2025-06-10 23:46:29 +0800

    update clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script'
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -19,6 +20,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh">
+      <head>
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "rxdgpwxh3v");
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         {children}
         <GoogleAnalytics gaId="G-LWBETX65FP" />
